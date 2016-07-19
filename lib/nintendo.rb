ff423fbd7fbf8460c11c1555a2ee1069bd9bbb47
funnythingz <funnythingz69+github@gmail.com>
2016-07-19 20:04:24 +0900

    スクレイピングはじめた

--- lib/nintendo.rb
+++ lib/nintendo.rb
@@ -1,14 +1,23 @@
 require "nintendo/version"
-require "nokogiri/html"
+require "nokogiri"
+require "open-uri"
 
 module Nintendo
   class Nintendo
+    def info
+      # TODO: なんか返す
+    end
+
     def stock_code
       '7974'
     end
 
     def stock_price
       # TODO: 株価をスクレイピングして返す
+      doc = Nokogiri::HTML(open('http://minkabu.jp/stock/7974'))
+      doc.css('.stock_price').each do |price|
+        price.content
+      end
     end
   end
 end
